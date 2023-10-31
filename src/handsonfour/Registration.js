3bab39f0b2e9b29ce0971955d501907fb0c36ea9
Harkirpa <kirpa704@gmail.com>
2023-10-31 18:40:11 +0530

    handson

--- src/handsonfour/Registration.js
+++ src/handsonfour/Registration.js
@@ -36,16 +36,16 @@ function Registration() {
    <>
    <form>
             <div className='formsnam'>
-            <input type='text'  placeholder='Name' name='name' value={data.name} onChange={handleInput} />
+            <input type='text'  placeholder='Name' name='name' id='name' value={data.name} onChange={handleInput} />
             </div>
             <div className='emailform'>
-                  <input type='text' placeholder='Email' name='email' value={data.email} onChange={handleInput} />
+                  <input type='text' placeholder='Email' name='email' id='email' value={data.email} onChange={handleInput} />
                   </div>
                   <div className='telno'>
-                  <input type='tel' placeholder='Phoneno' name='phone' value={data.phone} onChange={handleInput} />
+                  <input type='tel' placeholder='Phoneno' name='phone' id='phone' value={data.phone} onChange={handleInput} />
                   </div>
                   <div className='formpa'>
-                  <input type='password'  placeholder='Password' name='password' value={data.password} onChange={handleInput} /> 
+                  <input type='password'  placeholder='Password' name='password' id='password' value={data.password} onChange={handleInput} /> 
                   </div>
             <button type='btn' id='go' onClick={submit}>Register</button>
         </form>
